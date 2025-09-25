708896bd9ef755440cd5cf3387685178f3a49d29
Vanessa Barsotti <vaplacencio@hotmail.com>
2025-09-25 15:48:18 -0400

    Toasting added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 const rootElement = document.getElementById("root");
-
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
