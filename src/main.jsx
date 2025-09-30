6c24c0a35d40dc5f6b832f72fe179f89310b2c3a
Vanessa Barsotti <vaplacencio@hotmail.com>
2025-09-30 16:59:48 -0400

    Implement auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,43 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+import { msalConfig } from "./auth/authConfig";
+import { MsalProvider } from "@azure/msal-react";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-  console.error("Root element not found");
+const msalInstance = new PublicClientApplication(msalConfig);
+
+async function bootstrap() {
+  await msalInstance.initialize();
+
+  if (
+    !msalInstance.getActiveAccount() &&
+    msalInstance.getAllAccounts().length > 0
+  ) {
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+  }
+
+  msalInstance.enableAccountStorageEvents();
+
+  msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload?.account) {
+      msalInstance.setActiveAccount(event.payload.account);
+    }
+  });
+
+  const rootElement = document.getElementById("root");
+  if (rootElement) {
+    createRoot(rootElement).render(
+      <StrictMode>
+        <MsalProvider instance={msalInstance}>
+          <App />
+        </MsalProvider>
+      </StrictMode>
+    );
+  } else {
+    console.error("Root element not found");
+  }
 }
+
+bootstrap();
